8fb8cac16ec325c3f0f165d8a2ad1ea0297103ac
heifade <heifade@126.com>
2019-04-08 14:53:02 +0800

    no message

---
+++ config/config.ts
@@ -0,0 +1,55 @@
+// https://umijs.org/config/
+
+const plugins = [
+  [
+    'umi-plugin-react',
+    {
+      antd: true,
+      dva: {
+        hmr: true,
+        immer: true,
+      },
+      locale: {
+        enable: false, // default false
+        default: 'zh-CN', // default zh-CN
+        baseNavigator: true, // default true, when it is true, will use `navigator.language` overwrite default
+      },
+    },
+  ],
+];
+
+export default {
+  // add for transfer to umi
+  plugins,
+  treeShaking: true,
+  hash: true,
+  ignoreMomentLocale: true,
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+  },
+  disableRedirectHoist: true,
+  extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+  cssLoaderOptions: {
+    modules: true,
+    getLocalIdent: (context: any, localIdentName: any, localName: any) => {
+      return localName;
+      // if (
+      //   context.resourcePath.includes('node_modules') ||
+      //   context.resourcePath.includes('ant.design.pro.less') ||
+      //   context.resourcePath.includes('global.less')
+      // ) {
+      //   return localName;
+      // }
+      // const match = context.resourcePath.match(/src(.*)/);
+      // if (match && match[1]) {
+      //   const antdProPath = match[1].replace('.less', '');
+      //   const arr = slash(antdProPath)
+      //     .split('/')
+      //     .map(a => a.replace(/([A-Z])/g, '-$1'))
+      //     .map(a => a.toLowerCase());
+      //   return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+      // }
+      // return localName;
+    },
+  },
+};
