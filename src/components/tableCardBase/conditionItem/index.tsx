d244610a3c8bcdce3b8259db12b2324a7809bf01
heifade <heifade@126.com>
2019-04-23 17:15:53 +0800

    条件里的组件,可以传placeholder

--- src/components/tableCardBase/conditionItem/index.tsx
+++ src/components/tableCardBase/conditionItem/index.tsx
@@ -8,17 +8,19 @@ interface IProps {
   component?: any;
   initialValue?: any;
   visible?: boolean;
+  placeholder?: string;
 }
 export { IConditionItem };
 
 export function ConditionItem(props: IProps): IConditionItem {
-  const { title, field, initialValue = '', component, visible = true } = props;
+  const { title, field, initialValue = '', component, visible = true, placeholder } = props;
+  const nplaceholder = placeholder || `请输入${title}`;
 
   return {
     title,
     field,
     initialValue,
     visible,
-    component: component || <Input placeholder={`请输入${title}`} />,
+    component: component || <Input placeholder={nplaceholder} />,
   };
 }
